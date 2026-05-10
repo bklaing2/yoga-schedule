6821052dcf8d73936d83909ba86ebd95db90efb9
Bryceson Laing <bklaing2@gmail.com>
2026-05-10 11:49:49 -0500

    add stripes to tables

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -122,8 +122,8 @@ function Day(props: { date: Date, classes: Class[] }) {
       <span class="font-light text-gray-400"> | {props.classes.length} classes</span>
     </h3>
     <ul class="contents">
-      <For each={props.classes}>{(c) =>
-        <li class={`col-span-full grid grid-cols-subgrid px-4 py-0.5`}>
+      <For each={props.classes}>{(c, i) =>
+        <li class={`col-span-full grid grid-cols-subgrid ${i() % 2 !== 0 ? "bg-lagoon/5" : ""} px-4 py-0.5`}>
           <ClassItem {...c} />
         </li>
       }</For>
